4c8ed936e4000483e042b4ee7d0f43d68e13cb9b
Santiago Enzo Nisi <santiagonisi5@gmail.com>
2025-08-05 09:26:16 -0300

    new run

--- run.py
+++ run.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-# from yourapplication import routes
-# Update the import below to match your actual routes module, for example:
-# from routes import *
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    db.init_app(app)
+
+    from logistica.routes import main  # Importa el blueprint
+    app.register_blueprint(main)       # Registra el blueprint
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
